60be92206b17e0061553e936b120309fde930b52
137819913@qq.com <yzjqq995>
2019-07-15 06:45:10 +0800

    ribbon

---
+++ sun-rain-cloud-ribbon/src/main/java/com/sunrain/CloudRibbonApplication.java
@@ -0,0 +1,17 @@
+package com.sunrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yzz
+ */
+@SpringBootApplication
+public class CloudRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudRibbonApplication.class, args);
+    }
+
+
+}
