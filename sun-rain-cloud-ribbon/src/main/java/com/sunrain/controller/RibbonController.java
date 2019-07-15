7c30afef3982ef6adafa456deaa9552d1f7f9330
于振京 <yuzhenjing@ydfinance.com.cn>
2019-07-15 08:48:55 +0800

    ribbon

---
+++ sun-rain-cloud-ribbon/src/main/java/com/sunrain/controller/RibbonController.java
@@ -0,0 +1,19 @@
+package com.sunrain.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 服务注册与发现从测试
+ *
+ * @author admin
+ */
+@RestController
+public class RibbonController {
+
+
+
+    
+
+
+
+}
